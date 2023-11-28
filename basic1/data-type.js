867fa0283f9e24ee5386a806f6092a1d31602b3a
ajaysah00 <144298108+ajaysah00@users.noreply.github.com>
2023-11-28 13:48:28 +0000

    discussion on data-type

---
+++ basic1/data-type.js
@@ -0,0 +1,47 @@
+// Primitive (Stack)
+/*
+string
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+*/
+const firstname = "Ajays sah"
+const age = 25
+const score = 200.9
+let isLoggedIn = true
+const nodeversion = null
+let bankbalance = undefined
+const buttonId = Symbol("id")
+const aReallyLongNumber = 9845875475n
+
+//Reference (Non-primitive) -objects (Heap)
+/*
+Arrays
+objects
+Functions
+*/
+const Numbers = [1,2,3,4,5,6]
+
+const userObject = {
+    name: "Ajay sah",
+    age: 25,
+    isLoggedId: true
+}
+const heroes = [
+"superman",
+ "batman", 
+ "flash",
+ true,
+ 123
+]
+
+// Function 
+const sayHello = function(){
+    console.log{"AJay"}
+} 
+
+const registerAUser = function(){}
+const loginUser = function(){}
