89e436790fa178730bd6c05161cf282a68d93b16
ajaysah00 <144298108+ajaysah00@users.noreply.github.com>
2023-11-29 04:28:10 +0000

    discussion on operators

---
+++ basic1/operators.js
@@ -0,0 +1,23 @@
+// Arthmetic operators
+
+// 5+5
+// 10-5
+// 5*5
+// 10/2
+
+const numberone = "4"
+const numbertwo = "4"
+
+const numberthree = numberone + numbertwo
+
+console.log(numberthree);
+
+//Assignment operators
+let valueTwo = 3
+valueTwo +=10
+
+//comparision operators
+let pw = 3
+let pwskill = 3
+
+console.log(pw == pwskill)
