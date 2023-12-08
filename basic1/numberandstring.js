4bc54810e4732242b4019360edd98cae872dd4c4
ajaysah00 <144298108+ajaysah00@users.noreply.github.com>
2023-12-08 10:23:47 +0000

    String discussion

---
+++ basic1/numberandstring.js
@@ -0,0 +1,18 @@
+const firstName = 'Ajay';
+const lastName = " sah";
+
+console.log(firstName + lastName);
+console.log('my firstname is ${firstName} and my lastname is {lastName}');
+
+const username = new String('Ajay kumar sah');
+console.log(username [0]);
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.substring(0, 4));
+
+let email = "ajay@aj.com   ";
+console.log(email.trim());
+
+let email2 = "ajay@sah.com";
+console.log(email2.replace('ajay', "bijay"));
+console.log(email.includes('ajay'))
