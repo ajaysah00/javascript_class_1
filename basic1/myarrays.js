fe6a462c6d11ebc10be94b3b6671760ea58ac324
ajaysah00 <144298108+ajaysah00@users.noreply.github.com>
2023-12-14 13:36:21 +0000

    discussion about arrays

---
+++ basic1/myarrays.js
@@ -0,0 +1,15 @@
+const myArray = [1, 2, 3, 4, 5];
+
+const myNewArray = new Array(1, 2, 3, 4, 5)
+console.log(myArray[1]);
+
+const dcHeros = ["batman", "superman", "spiderman", "ironman"];
+console.log(dcHeros.length);
+
+// dcHeros[dcHeros.length] = "wonderman";
+dcHeros.push("wonderman")
+console.log(dcHeros)
+const LastHeros = dcHeros.pop();
+console.log(dcHeros);
+dcHeros.unshift("aquaman");
+console.log(dcHeros);
