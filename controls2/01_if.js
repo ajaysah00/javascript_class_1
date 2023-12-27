e4383ae8684b6db8c10a3826cff3c6ad51d59dcc
ajaysah00 <144298108+ajaysah00@users.noreply.github.com>
2023-12-27 12:44:03 +0000

    if and else discussion

--- controls2/01_if.js
+++ controls2/01_if.js
@@ -20,3 +20,34 @@ else{
     console.log("you have'nt got life");
 }
 console.log(`your score is ${score} and my life is ${life}`)
+
+
+// 1 star - poor
+// 2 star - emm, ok
+// 3 star - average
+// 4 star - cool
+// 5 star - great
+// 6 star - awsome
+// 7 star - nailed it
+
+
+let getStarFromUser = "9"
+let starInNumber = parseInt(getStarFromUser)
+
+if (starInNumber == 1){
+    console.log("poor");
+} else if (starInNumber == 2){
+    console.log("emm, ok")
+} else if (starInNumber == 3){
+    console.log("average")
+} else if (starInNumber == 4){
+    console.log("cool")
+} else if (starInNumber == 5){
+console.log("great")
+} else if (starInNumber == 6){
+    console.log("awsome")
+} else if (starInNumber == 7){
+    console.log("nailed it")
+} else {
+    console.log("I don't known that rating")
+}
